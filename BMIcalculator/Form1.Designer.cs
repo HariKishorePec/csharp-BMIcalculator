50c397bdd4d01f9d93b6e1926c7b4e9eee4d6bcb
Hari Kishore <HariKishorePec@users.noreply.github.com>
2018-08-22 11:17:29 +0530

    A basic GUI based BMI calc

---
+++ BMIcalculator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace BMIcalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblBmi = new System.Windows.Forms.Label();
+            this.lblComment = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(93, 68);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(81, 17);
+            this.lblWeight.TabIndex = 0;
+            this.lblWeight.Text = "Weight(kg):";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(93, 116);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(121, 17);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "Height(in meters):";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(295, 68);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(162, 22);
+            this.txtWeight.TabIndex = 2;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(295, 111);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(162, 22);
+            this.txtHeight.TabIndex = 3;
+            // 
+            // lblBmi
+            // 
+            this.lblBmi.AutoSize = true;
+            this.lblBmi.Location = new System.Drawing.Point(93, 164);
+            this.lblBmi.Name = "lblBmi";
+            this.lblBmi.Size = new System.Drawing.Size(43, 17);
+            this.lblBmi.TabIndex = 4;
+            this.lblBmi.Text = "BMI : ";
+            // 
+            // lblComment
+            // 
+            this.lblComment.AutoSize = true;
+            this.lblComment.Location = new System.Drawing.Point(93, 205);
+            this.lblComment.Name = "lblComment";
+            this.lblComment.Size = new System.Drawing.Size(75, 17);
+            this.lblComment.TabIndex = 5;
+            this.lblComment.Text = "Comment :";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(351, 270);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(106, 32);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "&Check Now";
+            this.btnSubmit.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(230, 270);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 32);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(96, 270);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 32);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(56, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 292);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 359);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblComment);
+            this.Controls.Add(this.lblBmi);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblBmi;
+        private System.Windows.Forms.Label lblComment;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BMIcalculator/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIcalculator
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ht, wt, BMI=0;
+            
+            if (txtWeight.Text != "" && txtHeight.Text != "")
+            {
+                wt = double.Parse(txtWeight.Text);
+                ht = double.Parse(txtHeight.Text);
+
+                BMI = Math.Round(wt / (ht * ht));
+                lblBmi.Text += BMI;
+
+                if (BMI > 25.0)
+                {
+                    lblComment.Text += " Overweight BMI.";
+                }
+                else if (BMI < 18.5)
+                {
+                    lblComment.Text += "Underweight BMI.";
+                }
+                else
+                {
+                    lblComment.Text += "Normal BMI.";
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Height and/or Weight cant not be empty!", "Error Message", MessageBoxButtons.OK);
+                ClrResult();
+            }
+
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtWeight.Text = "";
+            txtHeight.Text = "";
+            lblBmi.Text = "BMI :";
+            lblComment.Text = "Comment :";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void ClrResult()
+        {
+            lblBmi.Text = "BMI :";
+            lblComment.Text = "Comment :";
+        }
+    }
+}
